9a17217d93a277c9b686410b1daa93c059cb8bf3
HellHood <szymek0107@gmail.com>
2022-01-27 18:51:56 +0100

    php

---
+++ elementy/footer.php
@@ -0,0 +1,18 @@
+<footer>
+   <div id='about'>
+      <ul id='wszystko'>
+        <ul>
+          <li>O nas</li>
+        </ul>
+        <ul>
+          <li>Regulamin</li>
+        </ul>
+         <ul>
+           <li>Polityka prywatności</li>
+         </ul>
+         <ul>
+           <li>Kontakt</li>           
+         </ul>
+       </ul>
+   </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <link rel="Stylesheet" type="text/css" href="css/style.css">
+    <title>Nyggaware</title>
+</head>
+<body>
+ <?php
+include 'elementy/header.php' ;
+ ?>
+    <br>
+    <br>
+    <br>
+    <br>
+<?php
+include 'elementy/kategorie.php' ;
+?>
+    <br>
+    <br>
+    <br>
+    <h1>Popularne</h1>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+    <div id='rzeczy_sprzedarz'>
+      <ul id="lista_rzeczy">
+        <div class="przedmiot">
+          
+          <li>"Nazwa przedmiotu"</li>
+          <br>
+          <li><img src='img/iphone13.png'style="max-width:100%;height:auto;"></li>
+          <li><a href='items/Strona przedmiotu.html'>Zobacz więcej</a></li>
+          
+        </div>
+        <br>
+        <div class="przedmiot">
+          
+          <li>"Nazwa przedmiotu"</li>
+          <br>
+          <li><img src='img/iphone13.png' style="max-width:100%;height:auto;"></li>
+          <li>Zobacz więcej</li>
+          
+        </div>
+        <br>
+        <div class="przedmiot">
+        
+          <li>"Nazwa przedmiotu"</li>
+          <br>
+          <li><img src='img/iphone13.png' style="max-width:100%;height:auto;"></li>
+          <li>Zobacz więcej</li>
+        
+        </div>
+        <br>
+        <div class="przedmiot">
+          
+          <li>"Nazwa przedmiotu"</li>
+          <br>
+          <li><img src='img/iphone13.png'  width=' 200px' height=' 250px'></li>
+          <li>Zobacz więcej</li>
+          
+        </div>
+        <br>
+        <div class="przedmiot">
+          
+          <li>"Nazwa przedmiotu"</li>
+          <br>
+          <li><img src='img/iphone13.png' style="max-width:100%;height:auto;"></li>
+          <li>Zobacz więcej</li>
+          
+        </div>
+        <br>
+      </ul>
+    </div>
+    <aside>
+      Miejsce na reklame
+      <br>
+      217px na 650px
+    </aside>
+    </div>
+<?php
+include 'elementy/przycisk.php' ;
+?>
+<?php
+include 'elementy/footer.php' ;
+?>
+</div>
+</body>
+</html>
