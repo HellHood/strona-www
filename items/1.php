56ca4afee3c8374b5520a9fe355e48e3a2410573
HellHood <szymek0107@gmail.com>
2022-03-07 09:34:25 +0100

    gfd

--- items/1.php
+++ items/1.php
@@ -2,8 +2,8 @@
 if (isset($_GET['ID'])) {
   include 'elementy/baza.php';
     $id = preg_replace('#[^0-9]#i', '', $_GET['ID']);
-    $sql = mysql_query("SELECT * FROM products WHERE id='$id' LIMIT 1");
-    $productCount = mysql_num_rows($sql);
+    $sql = mysqli_query("SELECT * FROM products WHERE id='$id' LIMIT 1");
+    $productCount = mysqli_num_rows($sql);
   if ($productCount > 0) {
           // get all the product details
           while($row = mysql_fetch_array($sql)){ 
@@ -11,13 +11,13 @@ if (isset($_GET['ID'])) {
              $price = $row["cena"];
              $details = $row["opis"];
              $image = $row["zdjecie"];
-               };
+               }
            
         } else {
           echo "That item does not exist.";
             exit();
-        };
-};
+        }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
