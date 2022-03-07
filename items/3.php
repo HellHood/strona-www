1632d53442a691c8d7cc667d80ea577e3c8ab69a
HellHood <szymek0107@gmail.com>
2022-03-07 16:58:07 +0100

    afgs

---
+++ items/3.php
@@ -0,0 +1,93 @@
+<?php
+if (isset($_GET['id'])) {
+  include '../baza.php';
+    $id = preg_replace('#[^0-9]#i', '', $_GET['id']);
+    $sql = mysqli_query($conn, "SELECT * FROM products WHERE id='$id' LIMIT 1");
+    $productCount = mysqli_num_rows($sql);
+  if ($productCount > 0) {
+          // get all the product details
+          while($row = mysql_fetch_array($sql)){ 
+             $product_name = $row["nazwa"];
+             $price = $row["cena"];
+             $details = $row["opis"];
+             $image = $row["zdjecie"];
+               }
+           
+        } else {
+          echo "That item does not exist.";
+            exit();
+        }
+      } else {
+        echo "Data to render this page is missing.";
+        exit();
+}
+mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $product_name; ?></title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/item.css">
+</head>
+<body>
+<?php
+include '../elementy/gora.php' ;
+?>
+    <div class='item'>
+        <div class="obraz"><img src='<?php echo $image; ?>' alt="iphone13" width="500" height="600"></div>
+        <div class='podst_inf'>
+          <div class='opis'><?php echo $details; ?></div>
+          <br>
+          <p><?php echo $price ; ?></p>
+          <center><button class="dodaj-koszyk">Dodaj do koszyka</button></center>
+        </div>
+    </div>
+      <center><h1>Polecane produkty</h1></center>
+    <center>
+      <div class='polecane'>
+      <div><a href=""><img src="../img/iphone13.png" width="180px" height="250"></a></div>
+      <div><a href=""><img src="../img/iphone13.png" width="180px" height="250"></a></div>
+      <div><a href=""><img src="../img/iphone13.png" width="180px" height="250"></a></div>
+      <div><a href=""><img src="../img/iphone13.png" width="180px" height="250"></a></div>
+    </div>
+    </center>
+    <button onclick="topFunction()" id="myBtn" title="Go to top"><img src="../img/arrow.png" width="50px" height="50px"><br>Do góry</button>
+    <script>
+      var mybutton = document.getElementById("myBtn");
+      window.onscroll = function() {scrollFunction()};
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+      function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+      }
+    </script>
+    <footer> 
+        <div id='about'>
+          <ul id='wszystko'>
+            <ul>
+              <li>O nas</li>
+            </ul>
+            <ul>
+              <li>Regulamin</li>
+            </ul>
+            <ul>
+              <li>Polityka prywatności</li>
+            </ul>
+            <ul>
+              <li>Kontakt</li>           
+            </ul>
+          </ul>
+        </div>
+      </footer>
+</body>
+</html>
