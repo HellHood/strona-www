05405c3ba9580fc3dae89eb33e8d5160c9cade29
HellHood <szymek0107@gmail.com>
2022-05-19 19:33:46 +0200

    fas

--- elementy/header.php
+++ elementy/header.php
@@ -1,5 +1,5 @@
 <header>
-    <div id="Tytul"><a href="index.php"><img src="img/logo.png"  style="max-width:100%;height:auto;" alt='logo'></a></div>
+    <div id="Tytul"><a href="index.php"><img src="../img/logo.png"  style="max-width:100%;height:auto;" alt='logo'></a></div>
     <div id="login"><a href='logowanie/logowanie.php'>Login</a></div>
     <div id="konto"><a href="konto.php">Twoje konto</a></div>
     <div id='koszyk'><img src="img/koszyk ikona.png" width="25px" height="25px">    Koszyk</div>
